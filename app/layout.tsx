c39066f6b4348a6eccc8cc41cd52957f6a5a3594
Osleyder Gonzalez Acosta <osleyder.gonzalez@gmail.com>
2023-05-03 01:31:04 -0400

    Install react-icons in Project
    Add Navbar Component
     - Add Logo Component in Navbar
     - Add Search Component in Navbar
     - Add UserMenu Component in Navbar
    
    Add Avatar Component
    Add Container Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from "next/font/google";
 
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata = {
   title: "Airbnb",
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
       </body>
     </html>
