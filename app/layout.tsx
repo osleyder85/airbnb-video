e6ed580a95ad1f5c5d59df17f531bf34bb2cd1e0
Osleyder Gonzalez Acosta <osleyder.gonzalez@gmail.com>
2023-05-03 17:39:39 -0400

    Install Axios, Zustand, React-Hook-Form and React-Hot-Toast
    Add Components:
    - Button.tsx
    - Heading.tsx
    - Input.tsx
    - Modal.tsx
    - RegisterModal
    
    Add Hooks
    - useRegisterModal.ts
    
    Add Provider
    - ToasterProvider.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "Airbnb",
@@ -22,6 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
