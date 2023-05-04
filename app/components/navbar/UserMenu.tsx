bf98d5865dd0f1550a259b3736adbdae68c8c96a
Osleyder Gonzalez Acosta <osleyder.gonzalez@gmail.com>
2023-05-04 00:55:19 -0400

    Add function getCurrentUser()
    Add more MenuIten for UserMenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,8 +7,16 @@ import MenuItem from "./MenuItem";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser?: User | null
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
@@ -77,6 +85,35 @@ const UserMenu = () => {
           "
         >
           <div className="flex flex-col cursor-pointer">
+          {currentUser ? (
+            <>
+              <MenuItem
+                onClick={() => {}}
+                label="My trips"
+              />
+              <MenuItem
+                onClick={() => {}}
+                label="My favorites"
+              />
+              <MenuItem
+                onClick={() => {}}
+                label="My reservations"
+              />
+              <MenuItem
+                onClick={() => {}}
+                label="My properties"
+              />
+              <MenuItem
+                onClick={() => {}}
+                label="Airbnb my home"
+              />
+              <hr />
+              <MenuItem
+                onClick={() => signOut()}
+                label="Logout"
+              />
+            </>
+          ) : (
             <>
               <MenuItem
                 onClick={loginModal.onOpen}
@@ -87,6 +124,7 @@ const UserMenu = () => {
                 label="Sign up"
               />
             </>
+            )}
           </div>
         </div>
       )}      
